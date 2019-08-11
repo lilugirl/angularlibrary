f13fa4f36723c0019dc41d3fc34634a59e43cf88
lilu <lilugirl2005@126.com>
2019-08-11 21:17:03 +0800

    init

---
+++ projects/weather/src/lib/weather.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-weather',
+  template: `
+    <p>
+      weather works!
+    </p>
+  `,
+  styles: []
+})
+export class WeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
